19aea74a92273cc759a30a1384d0b50dfc28e3cd
Suraj kumar <surajkumar54985@gmail.com>
2022-02-14 16:35:04 +0530

    function declaration type changed to arrow functions

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id:'i1',
