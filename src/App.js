e72b225596a6cba7807c41bb197293beecde3653
Suraj kumar <surajkumar54985@gmail.com>
2022-02-14 16:27:59 +0530

    files arranged in directory structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
   const expenses = [
